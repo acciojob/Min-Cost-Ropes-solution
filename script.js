1be9f674970a6808c9cd4045d84bda7d41131772
iamgope <amangope005@gmail.com>
2023-02-13 15:26:13 +0530

    added

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your code here
+function mincost(arr)
+{
+  //write your code here
+    arr.sort((a,b)=>a-b)
+
+    let cost = 0
+    //console.log(arr)
+    while(arr.length>1){
+        let first = arr.shift()
+        let second = arr.shift()
+
+        cost+=first+second
+
+        arr.push(first+second)
+        arr.sort((a,b)=>a-b)
+      //  console.log(arr)
+    }
+
+    return cost
+}
+
+module.exports=mincost;
